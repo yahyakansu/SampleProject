fdbf38d615ec9a23f7c3f69faf92a63f54239bd3
yahyakansu <yahyakansusa@gmail.com>
2020-12-01 19:27:06 -0600

    try the connection for chrome

---
+++ src/test/java/test/try1.java
@@ -0,0 +1,15 @@
+package test;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class try1 {
+    public static void main(String[] args) {
+        WebDriverManager.chromedriver().setup();
+        WebDriver driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.get("https://www.google.com");
+        driver.close();
+    }
+}
