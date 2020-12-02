74010ebe7eb4fae0a422f7952c6c0df49f94d4c3
yahyakansu <yahyakansusa@gmail.com>
2020-12-02 14:52:05 -0600

    solved the notication issue for chrome

--- src/test/java/test/try1.java
+++ src/test/java/test/try1.java
@@ -1,15 +1,27 @@
 package test;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+
+import java.util.concurrent.TimeUnit;
 
 public class try1 {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
         WebDriverManager.chromedriver().setup();
-        WebDriver driver = new ChromeDriver();
+        ChromeOptions opt = new ChromeOptions();
+        opt.addArguments("--disable-notifications");
+        WebDriver driver = new ChromeDriver(opt);
         driver.manage().window().maximize();
-        driver.get("https://www.google.com");
-        driver.close();
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+
+        driver.get("https://uenjoy.com/");
+//        Actions actions = new Actions(driver);
+//        actions.moveToElement(driver.findElement(By.linkText("Uenjoy Ride-ons")));
+//        actions.moveToElement(driver.findElement(By.partialLinkText("Racer"))).click();
+        Thread.sleep(1000);
+//        driver.close();
     }
 }
