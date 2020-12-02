abeb8e4a68d5cee3c54eb45c5b46946477d3681b
yahyakansu <yahyakansusa@gmail.com>
2020-12-02 15:03:31 -0600

    solved the notication issue for firefox

--- src/test/java/test/try1.java
+++ src/test/java/test/try1.java
@@ -5,15 +5,23 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.firefox.FirefoxOptions;
 
 import java.util.concurrent.TimeUnit;
 
 public class try1 {
     public static void main(String[] args) throws InterruptedException {
-        WebDriverManager.chromedriver().setup();
-        ChromeOptions opt = new ChromeOptions();
-        opt.addArguments("--disable-notifications");
-        WebDriver driver = new ChromeDriver(opt);
+//        WebDriverManager.chromedriver().setup();
+//        ChromeOptions opt = new ChromeOptions();
+//        opt.addArguments("--disable-notifications");
+//        WebDriver driver = new ChromeDriver(opt);
+
+        WebDriverManager.firefoxdriver().setup();
+        FirefoxOptions opt = new FirefoxOptions();
+        opt.addPreference("dom.webnotifications.enabled",false);
+        WebDriver driver = new FirefoxDriver(opt);
+
         driver.manage().window().maximize();
         driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
 
@@ -22,6 +30,6 @@ public class try1 {
 //        actions.moveToElement(driver.findElement(By.linkText("Uenjoy Ride-ons")));
 //        actions.moveToElement(driver.findElement(By.partialLinkText("Racer"))).click();
         Thread.sleep(1000);
-//        driver.close();
+        driver.close();
     }
 }
