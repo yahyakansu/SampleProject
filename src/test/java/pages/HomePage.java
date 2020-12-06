3c8c1fff0009bd801b93a2e0a6a5fc88f36bade2
yahyakansu <yahyakansusa@gmail.com>
2020-12-06 16:25:18 -0600

    created HomePage.class for POM

---
+++ src/test/java/pages/HomePage.java
@@ -0,0 +1,10 @@
+package pages;
+
+import org.openqa.selenium.support.PageFactory;
+import utilities.Driver;
+
+public class HomePage {
+    public HomePage(){
+        PageFactory.initElements(Driver.getDriver(),this);
+    }
+}
--- src/test/java/runners/CukesRunner.java
+++ src/test/java/runners/CukesRunner.java
@@ -4,18 +4,20 @@ import io.cucumber.junit.Cucumber;
 import io.cucumber.junit.CucumberOptions;
 import org.junit.AfterClass;
 import org.junit.runner.RunWith;
+import utilities.Driver;
 
 @RunWith(Cucumber.class)
 @CucumberOptions(
         plugin = {"html:target/cucumber-reports","json:target/cukesreport.json"},
         features = "src/test/resources/features",
         glue = "stepDefs",
-        dryRun = true,
+        dryRun = false,
         tags = "@Ride-ons"
 )
 public class CukesRunner {
     @AfterClass
     public static void tearDown(){
 //        ExtentReport.endReport();
+        Driver.closeDriver();
     }
 }
