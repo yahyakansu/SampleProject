bface1f36aa93f45c03052c28ac356d15745390a
yahyakansu <yahyakansusa@gmail.com>
2020-12-05 23:35:44 -0600

    created CukesRunner.class

--- src/test/java/runners/CukesRunner.java
+++ src/test/java/runners/CukesRunner.java
@@ -1,4 +1,21 @@
 package runners;
 
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+import org.junit.AfterClass;
+import org.junit.runner.RunWith;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+        plugin = {"html:target/cucumber-reports","json:target/cukesreport.json"},
+        features = {"src/java/resources/features"},
+        glue = {"stepDefs"},
+        dryRun = true,
+        tags = ""
+)
 public class CukesRunner {
+    @AfterClass
+    public static void tearDown(){
+//        ExtendReport.endReport();
+    }
 }
