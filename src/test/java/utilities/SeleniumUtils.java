6a1ca65c6acfeebd79bdd2835b4aaef7d18b84c3
yahyakansu <yahyakansusa@gmail.com>
2020-12-07 22:34:22 -0600

    s

--- src/test/java/utilities/SeleniumUtils.java
+++ src/test/java/utilities/SeleniumUtils.java
@@ -1,2 +1,26 @@
-package utilities;public class SeleniumUtils {
+package utilities;
+
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.Select;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.util.ArrayList;
+import java.util.List;
+public class SeleniumUtils {
+
+    public static List<String> getAllSelectedOptions(WebElement element){
+        Select select = new Select(element);
+        List<String> options = new ArrayList<>();
+
+        for (WebElement each:select.getOptions()){
+            options.add(each.getText());
+        }
+        return options;
+    }
+
+    public static void waitForVisibility(WebElement element,int seconds){
+        WebDriverWait wait = new WebDriverWait(Driver.getDriver(),seconds);
+        wait.until(ExpectedConditions.visibilityOf(element));
+    }
 }
