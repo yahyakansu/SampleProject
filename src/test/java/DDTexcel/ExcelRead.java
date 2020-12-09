ef75d042041ff7fcd24fac0728107695bedfbcd3
yahyakansu <yahyakansusa@gmail.com>
2020-12-08 22:00:15 -0600

    xlsx file existed

--- src/test/java/DDTexcel/ExcelRead.java
+++ src/test/java/DDTexcel/ExcelRead.java
@@ -1,5 +1,10 @@
 package DDTexcel;
 
-public class ExcelRead {
+import java.io.File;
 
+public class ExcelRead {
+    public static void main(String[] args) {
+        File file = new File("src/Sample.xlsx");
+        System.out.println(file.exists());
+    }
 }
